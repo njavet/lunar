86e01d0f15d3080a9b0af8b3aedba4988ee1e727
Noe Javet <np.javet@gmail.com>
2025-01-03 18:59:10 +0100

    change dqn

--- rla2048/fts/heuristics.py
+++ rla2048/fts/heuristics.py
@@ -1,78 +1,30 @@
-from collections import defaultdict
 import numpy as np
 
 
-    def find_best_move(self, board: list[list[int]]) -> int:
-        self.state = np.array(board, dtype=np.int16)
-        result = np.array([self.score_top_level_move(i) for i in range(4)])
+def max_tile_heuristic(board: np.array) -> int:
+    return int(np.log2(np.max(board)))
 
-        if np.max(result) == 0:
-            move = int(np.random.choice([0, 1, 2, 3]))
-        else:
-            move = random_argmax(result)
-        return move
 
-    def score_top_level_move(self, move):
-        self.simulator.simulate_move(self.state, move)
-        if np.all(self.simulator.new_state == self.state):
-            return 0
+def zero_tile_heuristic(board: np.array) -> int:
+    return 16 - np.count_nonzero(board)
 
-        return self.expectimax(self.simulator.new_state, self.depth, agent_play=False)
 
-    def expectimax(self, grid: np.ndarray, depth: int, agent_play: bool) -> float:
-        if depth == 0:
-            return self.utility(grid)
+def smoothness_heuristic(board: np.array) -> float:
+    smoothness = 0
 
-        if agent_play:
-            alpha = 0
-            for move in range(4):
-                self.simulator.simulate_move(grid, move)
-                if not np.equal(self.simulator.new_state, grid).all():
-                    alpha = max(alpha, self.expectimax(self.simulator.new_state,
-                                                       depth-1,
-                                                       False))
-            return alpha
-        else:
-            expected_value = 0
-            zero_cells = [(i, j) for i, row in enumerate(grid)
-                          for j, val in enumerate(row) if val == 0]
-            zeros = len(zero_cells)
-
-            for i, j in zero_cells:
-                ng2 = copy.deepcopy(grid)
-                ng2[i][j] = 2
-                expected_value += 0.9 * self.expectimax(ng2, depth-1, True)
-
-                ng4 = copy.deepcopy(grid)
-                ng4[i][j] = 4
-                expected_value += 0.1 * self.expectimax(ng4, depth-1, True)
-            return (1 / zeros) * expected_value
-
-@staticmethod
-def max_tile_heuristic(grid: np.array):
-    return int(np.log2(np.max(grid)))
-
-@staticmethod
-def zero_tile_heuristic(grid: np.array):
-    return 16 - np.count_nonzero(grid)
-
-@staticmethod
-def smoothness_heuristic(grid: np.array):
-        smoothness = 0
-
-    for row in grid:
-        for i in range(len(row) - 1):
+    for row in board:
+        for i in range(3):
             if row[i] > 0 and row[i + 1] > 0:
                 smoothness += abs(row[i] - row[i + 1])
 
-    for col in grid.transpose():
-        for i in range(len(col) - 1):
+    for col in board.transpose():
+        for i in range(3):
             if col[i] > 0 and col[i + 1] > 0:
                 smoothness += abs(col[i] - col[i + 1])
     return smoothness
 
-@staticmethod
-def monotonicity_heuristic(grid: np.ndarray):
+
+def monotonicity_heuristic(grid: np.ndarray) -> float:
     monotonicity = 0
 
     def calculate_monotonicity(array):
@@ -81,7 +33,7 @@ def monotonicity_heuristic(grid: np.ndarray):
             if array[i] >= array[i + 1]:
                 decreasing += array[i] - array[i + 1]
             elif array[i] <= array[i + 1]:
-                    increasing += array[i + 1] - array[i]
+                increasing += array[i + 1] - array[i]
             return increasing, decreasing
 
     # Calculate monotonicity for rows
@@ -94,13 +46,14 @@ def monotonicity_heuristic(grid: np.ndarray):
         monotonicity += min(calculate_monotonicity(col))
     return monotonicity
 
-@staticmethod
-def corner_heuristic(grid: np.array):
+
+def corner_heuristic(board: np.array) -> int:
     weights = np.array([[8, 0, 0, 0],
                         [3, -1, -1, -1],
                         [2, -1, -1, -1],
                         [1, 1, -1, -1]])
-    return int(np.sum(grid * weights))
+    return int(np.sum(board * weights))
+
 
 def utility(self, grid: np.array) -> float:
     max_tile = self.max_tile_heuristic(grid)
@@ -109,9 +62,42 @@ def utility(self, grid: np.array) -> float:
     mono = self.monotonicity_heuristic(grid)
     corner = self.corner_heuristic(grid)
 
-    print('maxtile', max_tile,
-          'zeros', zeros,
-          'smooth', smooth,
-          'mono', mono,
-          'corner', corner)
-    return mono + corner + zeros + max_tile
+    return mono + corner + zeros + max_tile + smooth
+
+
+def old_utility(board: np.array) -> float:
+    def helper(seq: np.ndarray) -> float:
+        # number of zeros heuristic
+        zeros = np.sum(seq == 0)
+        # higher tiles are better
+        rank = np.max(seq)
+        if rank == 0:
+            rw = 1
+        else:
+            rw = 1 / rank
+
+        # large tiles on the edge
+        ind = np.where(seq == rank)[0][0]
+        if ind == 0 or ind == 3:
+            edge = 1 - rw
+        else:
+            edge = 0
+
+        # monotonous
+        mono = 0
+        mon_inc = np.all([val <= seq[i + 1] for i, val in enumerate(seq[:-1])])
+        mon_dec = np.all([seq[i + 1] <= val for i, val in enumerate(seq[:-1])])
+        if mon_inc:
+            mono += 2
+        if mon_dec:
+            mono += 2
+
+        adj = 0
+        for i, val in enumerate(seq[1:]):
+            if np.all(val == seq[i + 1]):
+                adj += 1 - rw
+
+        return zeros + edge + mono + adj
+
+    tmp = np.sum([helper(board[:, i]) for i in range(4)])
+    return tmp + np.sum([helper(board[i, :]) for i in range(4)])
