4bbff96a682615fdc8c9cb5a6cff12840d55a2c4
Noe Javet <np.javet@gmail.com>
2025-01-04 13:48:58 +0100

    update types

--- rla2048/fts/merge.py
+++ rla2048/fts/merge.py
@@ -1,7 +1,7 @@
-import numpy as np
+import torch
 
 
-def merge_left(board: np.ndarray) -> tuple[np.ndarray, float]:
+def merge_left(board: torch.Tensor) -> tuple[torch.Tensor, float]:
 
     def _merge_row(row: list, acc: list, s: float = 0) -> tuple[list[int], float]:
         if not row:
@@ -24,28 +24,28 @@ def merge_left(board: np.ndarray) -> tuple[np.ndarray, float]:
     for row_ in board:
         shifted_row = row_[row_ != 0].tolist()
         merged_row, s_ = _merge_row(shifted_row, acc=[])
-        padded_row = np.pad(merged_row, (0, 4 - len(merged_row)))
+        padded_row = torch.pad(merged_row, (0, 4 - len(merged_row)))
         new_board.append(padded_row)
         score += s_
-    return np.array(new_board, dtype=np.int64), score
+    return torch.array(new_board, dtype=torch.int64), score
 
 
-def merge_right(board: np.ndarray) -> tuple[np.ndarray, float]:
-    flipped = np.fliplr(board)
+def merge_right(board: torch.Tensor) -> tuple[torch.Tensor, float]:
+    flipped = torch.fliplr(board)
     left_flipped, score = merge_left(flipped)
-    new_board = np.fliplr(left_flipped)
+    new_board = torch.fliplr(left_flipped)
     return new_board, score
 
 
-def merge_down(board: np.ndarray) -> tuple[np.ndarray, float]:
-    rotated = np.rot90(board, -1)
+def merge_down(board: torch.Tensor) -> tuple[torch.Tensor, float]:
+    rotated = torch.rot90(board, -1)
     left_rotated, score = merge_left(rotated)
-    new_board = np.rot90(left_rotated)
+    new_board = torch.rot90(left_rotated)
     return new_board, score
 
 
-def merge_up(board: np.ndarray) -> tuple[np.ndarray, float]:
-    rotated = np.rot90(board)
+def merge_up(board: torch.Tensor) -> tuple[torch.Tensor, float]:
+    rotated = torch.rot90(board)
     left_rotated, score = merge_left(rotated)
-    new_board = np.rot90(left_rotated, -1)
+    new_board = torch.rot90(left_rotated, -1)
     return new_board, score
