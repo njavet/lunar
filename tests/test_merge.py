639c2623f617b620c3822749299e429658298a13
Noe Javet <np.javet@gmail.com>
2025-01-04 13:53:49 +0100

    update tests

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,5 +1,5 @@
 import unittest
-import numpy as np
+import torch
 
 # project imports
 from rla2048.fts import merge_left, merge_down, merge_right, merge_up
@@ -8,23 +8,23 @@ from rla2048.fts import merge_left, merge_down, merge_right, merge_up
 class TestMerge(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.grid0 = np.array([[2, 2, 2, 2],
-                              [4, 4, 0, 0],
-                              [2, 4, 8, 16],
-                              [2, 0, 2, 4]])
+        cls.grid0 = torch.tensor([[2, 2, 2, 2],
+                                  [4, 4, 0, 0],
+                                  [2, 4, 8, 16],
+                                  [2, 0, 2, 4]])
 
-        cls.grid1 = np.array([[2, 2, 4, 2],
-                              [8, 4, 4, 16],
-                              [8, 0, 0, 16],
-                              [0, 0, 0, 32]])
+        cls.grid1 = torch.tensor([[2, 2, 4, 2],
+                                  [8, 4, 4, 16],
+                                  [8, 0, 0, 16],
+                                  [0, 0, 0, 32]])
 
     def test_merge_left_grid0(self):
         grid, _ = merge_left(self.grid0)
-        grid_res = np.array([[4, 4, 0, 0],
-                             [8, 0, 0, 0],
-                             [2, 4, 8, 16],
-                             [4, 4, 0, 0]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[4, 4, 0, 0],
+                                 [8, 0, 0, 0],
+                                 [2, 4, 8, 16],
+                                 [4, 4, 0, 0]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
     def test_merge_left_score_grid0(self):
         _, score = merge_left(self.grid0)
@@ -32,11 +32,11 @@ class TestMerge(unittest.TestCase):
 
     def test_merge_left_grid1(self):
         grid, _ = merge_left(self.grid1)
-        grid_res = np.array([[4, 4, 2, 0],
-                             [8, 8, 16, 0],
-                             [8, 16, 0, 0],
-                             [32, 0, 0, 0]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[4, 4, 2, 0],
+                                 [8, 8, 16, 0],
+                                 [8, 16, 0, 0],
+                                 [32, 0, 0, 0]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
     def test_merge_left_score_grid1(self):
         _, score = merge_left(self.grid1)
@@ -44,27 +44,27 @@ class TestMerge(unittest.TestCase):
 
     def test_merge_right_grid0(self):
         grid, _ = merge_right(self.grid0)
-        grid_res = np.array([[0, 0, 4, 4],
-                             [0, 0, 0, 8],
-                             [2, 4, 8, 16],
-                             [0, 0, 4, 4]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[0, 0, 4, 4],
+                                 [0, 0, 0, 8],
+                                 [2, 4, 8, 16],
+                                 [0, 0, 4, 4]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
     def test_merge_right_grid1(self):
         grid, _ = merge_right(self.grid1)
-        grid_res = np.array([[0, 4, 4, 2],
-                             [0, 8, 8, 16],
-                             [0, 0, 8, 16],
-                             [0, 0, 0, 32]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[0, 4, 4, 2],
+                                 [0, 8, 8, 16],
+                                 [0, 0, 8, 16],
+                                 [0, 0, 0, 32]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
     def test_merge_up_grid0(self):
         grid, _ = merge_up(self.grid0)
-        grid_res = np.array([[2, 2, 2, 2],
-                             [4, 8, 8, 16],
-                             [4, 0, 2, 4],
-                             [0, 0, 0, 0]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[2, 2, 2, 2],
+                                 [4, 8, 8, 16],
+                                 [4, 0, 2, 4],
+                                 [0, 0, 0, 0]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
     def test_merge_up_score_grid0(self):
         _, score = merge_up(self.grid0)
@@ -72,11 +72,11 @@ class TestMerge(unittest.TestCase):
 
     def test_merge_up_grid1(self): 
         grid, _ = merge_up(self.grid1)
-        grid_res = np.array([[2, 2, 8, 2],
-                             [16, 4, 0, 32],
-                             [0, 0, 0, 32],
-                             [0, 0, 0, 0]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[2, 2, 8, 2],
+                                 [16, 4, 0, 32],
+                                 [0, 0, 0, 32],
+                                 [0, 0, 0, 0]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
     def test_merge_up_score_grid1(self):
         _, score = merge_up(self.grid1)
@@ -84,19 +84,19 @@ class TestMerge(unittest.TestCase):
 
     def test_merge_down_grid0(self):
         grid, _ = merge_down(self.grid0)
-        grid_res = np.array([[0, 0, 0, 0],
-                             [2, 0, 2, 2],
-                             [4, 2, 8, 16],
-                             [4, 8, 2, 4]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[0, 0, 0, 0],
+                                 [2, 0, 2, 2],
+                                 [4, 2, 8, 16],
+                                 [4, 8, 2, 4]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
     def test_merge_down_grid1(self):
         grid, _ = merge_down(self.grid1)
-        grid_res = np.array([[0, 0, 0, 0],
-                             [0, 0, 0, 2],
-                             [2, 2, 0, 32],
-                             [16, 4, 8, 32]])
-        self.assertTrue(np.array_equal(grid, grid_res))
+        grid_res = torch.tensor([[0, 0, 0, 0],
+                                 [0, 0, 0, 2],
+                                 [2, 2, 0, 32],
+                                 [16, 4, 8, 32]])
+        self.assertTrue(torch.equal(grid, grid_res))
 
 
 if __name__ == '__main__':
