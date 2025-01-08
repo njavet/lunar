b7ddefe64f0b7602167e838650a3447158f8b89d
Noe Javet <np.javet@gmail.com>
2025-01-08 08:53:34 +0100

    wip

--- rla2048/core/state.py
+++ rla2048/core/state.py
@@ -27,83 +27,63 @@ def board_to_state(board: np.ndarray) -> np.ndarray:
     return state
 
 
-def shift_left(state: np.ndarray) -> np.ndarray:
-
-    shifted_state = np.array([np.pad(row[row != 0],
+def shift_left(board: np.ndarray) -> np.ndarray:
+    # TODO shift and merge without converting to board
+    shifted_board = np.array([np.pad(row[row != 0],
                                      (0, len(row) - np.count_nonzero(row)))
-                              for grid in state for row in grid]).reshape(16, 4, 4)
-    return shifted_state
+                              for row in board])
+    return shifted_board
 
 
-def merge_left(board: np.ndarray) -> tuple[np.ndarray, float]:
-
-    def _merge_row(row: list, acc: list, s: float = 0) -> tuple[list[int], float]:
-        if not row:
-            return acc, s
-        x = row[0]
-        if len(row) == 1:
-            return acc + [x], s
-        if x == row[1]:
-            new_row = row[2:]
-            new_acc = acc + [2 * x]
-            new_r = s + 2 * x
-            return _merge_row(new_row, new_acc, new_r)
-        else:
-            new_row = row[1:]
-            new_acc = acc + [x]
-            return _merge_row(new_row, new_acc, s)
-
-    new_board = []
-    score = 0
-    for row_ in board:
-        shifted_row = row_[row_ != 0].tolist()
-        merged_row, s_ = _merge_row(shifted_row, acc=[])
-        padded_row = np.pad(merged_row, (0, 4 - len(merged_row)))
-        new_board.append(padded_row)
-        score += s_
-    return np.array(new_board), score
+def merge_left(board: np.ndarray) -> np.ndarray:
+    shifted = shift_left(board)
+    merged = shifted.copy()
+    mask = (shifted[:, :-1] == shifted[:, 1:]) & (shifted[:, :-1] != 0)
+    merged[:, :-1][mask] *= 2
+    merged[:, 1:][mask] = 0
+    return shift_left(merged)
 
 
-def merge_right(board: np.ndarray) -> tuple[np.ndarray, float]:
+def merge_right(board: np.ndarray) -> np.ndarray:
     flipped = np.fliplr(board)
-    left_flipped, score = merge_left(flipped)
+    left_flipped = merge_left(flipped)
     new_board = np.fliplr(left_flipped)
-    return new_board, score
+    return new_board
 
 
-def merge_down(board: np.ndarray) -> tuple[np.ndarray, float]:
+def merge_down(board: np.ndarray) -> np.ndarray:
     rotated = np.rot90(board, -1)
-    left_rotated, score = merge_left(rotated)
+    left_rotated = merge_left(rotated)
     new_board = np.rot90(left_rotated)
-    return new_board, score
+    return new_board
 
 
-def merge_up(board: np.ndarray) -> tuple[np.ndarray, float]:
+def merge_up(board: np.ndarray) -> np.ndarray:
     rotated = np.rot90(board)
-    left_rotated, score = merge_left(rotated)
+    left_rotated = merge_left(rotated)
     new_board = np.rot90(left_rotated, -1)
-    return new_board, score
+    return new_board
 
 
-def execute_action(board: np.ndarray, action: int) -> tuple[np.ndarray, float]:
+def execute_action(board: np.ndarray, action: int) -> np.ndarray:
     if action == 0:
-        new_board, score = merge_left(board)
+        new_board = merge_left(board)
     elif action == 1:
-        new_board, score = merge_down(board)
+        new_board = merge_down(board)
     elif action == 2:
-        new_board, score = merge_right(board)
+        new_board = merge_right(board)
     elif action == 3:
-        new_board, score = merge_up(board)
+        new_board = merge_up(board)
     else:
         raise ValueError(f'invalid action: {action}')
-    return new_board, score
+    return new_board
 
 
 def game_over(board: np.ndarray) -> bool:
     if np.any(board == 0):
         return False
     for action in Actions:
-        new_board, _ = execute_action(board, action.value)
+        new_board = execute_action(board, action.value)
         if not np.equal(board, new_board):
             return False
     return True
