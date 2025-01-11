279acb623a412f644ed3cc18ad4f44f79f2b04fe
Noe Javet <np.javet@gmail.com>
2025-01-11 14:46:20 +0100

    3 different nns

--- lunar/dqns.py
+++ lunar/dqns.py
@@ -1,17 +1,52 @@
 import torch.nn as nn
 
 
-class DQN(nn.Module):
-    def __init__(self, obs_dim=8, action_dim=4):
-        super(DQN, self).__init__()
-        self.fc0 = nn.Linear(obs_dim, 512)
-        self.fc1 = nn.Linear(512, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, action_dim)
+class LunarDQNSmall(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(8, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 4)
+        )
 
     def forward(self, x):
-        x = nn.functional.relu(self.fc0(x))
-        x = nn.functional.relu(self.fc1(x))
-        x = nn.functional.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        return self.net(x)
+
+
+class LunarDQNMiddle(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(8, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 4),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class LunarDQNLarge(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(8, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 4)
+        )
+
+    def forward(self, x):
+        return self.net(x)
