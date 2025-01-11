62d26b0973b6add66c35e47df120a1071dfcdb57
Noe Javet <np.javet@gmail.com>
2025-01-11 12:24:12 +0100

    copy files from 2048 repo

---
+++ lunar/agent.py
@@ -0,0 +1,132 @@
+from collections import deque
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class DQNAgent:
+    def __init__(self,
+                 dqn: nn.Module,
+                 gamma: float,
+                 epsilon: float,
+                 epsilon_min: float,
+                 decay: float,
+                 batch_size: int,
+                 memory_size: int,
+                 update_target_steps: int,
+                 lr: float) -> None:
+        self.dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.policy_net = dqn().to(self.dev)
+        self.target_net = dqn().to(self.dev)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.memory = ReplayMemory(self.dev, memory_size=memory_size)
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
+        self.batch_size = batch_size
+        self.update_target_steps = update_target_steps
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.decay = decay
+        self.steps = 0
+
+    def select_actions(self, states):
+        if random.random() < self.epsilon:
+            return np.random.randint(4, size=len(states))
+        states = torch.tensor(states, dtype=torch.float32, device=self.dev)
+        with torch.no_grad():
+            q_values = self.policy_net(states)
+        actions = q_values.argmax(dim=1).detach().cpu().numpy()
+        return actions
+
+    def learn(self):
+        self.steps += 1
+        if len(self.memory) < self.batch_size:
+            return
+        states, actions, rewards, next_states, dones = self.memory.sample(self.batch_size)
+        q_values = self.policy_net(states).gather(1, actions).squeeze()
+        with torch.no_grad():
+            next_q_values = self.target_net(next_states).max(1)[0]
+        expected_q_values = rewards + (self.gamma * next_q_values * (1 - dones))
+        loss = torch.nn.functional.mse_loss(q_values, expected_q_values)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.epsilon = max(self.epsilon * self.decay, self.epsilon_min)
+
+    def store_transitions(self, states, actions, rewards, next_states, dones):
+        self.memory.push(states, actions, rewards, next_states, dones)
+
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+
+class ReplayMemory:
+    def __init__(self, device: torch.device, memory_size: int):
+        self.device = device
+        self.memory = deque(maxlen=memory_size)
+
+    def push(self, states, actions, rewards, next_states, dones):
+        transitions = zip(states, actions, rewards, next_states, dones)
+        for transition in transitions:
+            self.memory.append(transition)
+
+    def sample(self, batch_size):
+        batch = random.sample(self.memory, batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+        states = torch.tensor(np.array(states),
+                              dtype=torch.float32,
+                              device=self.device)
+        actions = torch.tensor(np.array(actions),
+                               dtype=torch.long,
+                               device=self.device).unsqueeze(1)
+        rewards = torch.tensor(np.array(rewards),
+                               dtype=torch.float32,
+                               device=self.device)
+        next_states = torch.tensor(np.array(next_states),
+                                   dtype=torch.float32,
+                                   device=self.device)
+        dones = torch.tensor(np.array(dones),
+                             dtype=torch.float32,
+                             device=self.device)
+        return states, actions, rewards, next_states, dones
+
+    def __len__(self):
+        return len(self.memory)
+
+"""
+    def replay(self):
+        if len(self.memory) < self.batch_size:
+            return
+        batch = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, next_states, dones = map(
+            lambda x: torch.stack(x).to(self.device),
+            zip(*batch)
+        )
+
+        q_values = self.model(states)
+        with torch.no_grad():
+            next_q_values = self.target_model(next_states)
+            max_next_q_values = torch.max(next_q_values, dim=1)[0]
+
+        targets = q_values.clone()
+        for i in range(self.batch_size):
+            targets[i, actions[i]] = rewards[i] if dones[i] else rewards[i] + self.gamma * max_next_q_values[i]
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(q_values, targets)
+        loss.backward()
+        self.optimizer.step()
+
+    def save_checkpoint(self, episode, filename='checkpoint.pth'):
+        torch.save({
+            'model_state_dict': self.model.state_dict(),
+            'target_model_state_dict': self.target_model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'memory': self.memory,
+            'episode': episode,
+            'epsilon': self.epsilon
+        }, filename)
+
+"""
---
+++ lunar/main.py
@@ -0,0 +1,94 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium.vector import SyncVectorEnv
+from stable_baselines3.common.env_util import make_vec_env
+import torch
+import cv2
+
+# project imports
+from rla2048.config import Params
+from rla2048.agent import DQNAgent
+from rla2048.env import Env2048
+from rla2048.dqns import DQN, ConNet
+
+
+def main():
+    params = Params()
+    agent = DQNAgent(dqn=ConNet,
+                     gamma=params.gamma,
+                     epsilon=params.epsilon,
+                     epsilon_min=params.epsilon_min,
+                     decay=params.decay,
+                     batch_size=params.batch_size,
+                     memory_size=params.memory_size,
+                     update_target_steps=params.update_target_steps,
+                     lr=params.lr)
+    # env = make_vec_env('Game2048-v0', n_envs=16)
+    env = make_vec_env(lambda: Env2048(), n_envs=16)
+    # env = gym.make('Game2048-v0')
+    train_agent(agent, env)
+
+
+def train_agent(agent, env):
+    max_time_steps = 100000
+    states = env.reset()
+    episode_rewards = np.zeros(16)
+    for step in range(max_time_steps):
+        actions = agent.select_actions(states)
+        next_states, rewards, dones, infos = env.step(actions)
+        episode_rewards += rewards
+        agent.store_transitions(states, actions, rewards, next_states, dones)
+        agent.learn()
+        states = next_states
+        if any(dones):
+            current_rewards = [episode_rewards[i] for i in
+                               range(len(episode_rewards)) if dones[i]]
+            print(f'total rewards: ', np.mean(current_rewards))
+            print(f'eps', agent.epsilon)
+        if step % 1000 == 0:
+            agent.update_target_net()
+        if step % 100 == 0:
+            print(f'step: {step}, rewards: {np.mean(episode_rewards)}')
+    torch.save(agent.target_net.state_dict(), 'g2048.pth')
+
+
+def load_checkpoint(agent, filename='checkpoint.pth'):
+    checkpoint = torch.load(filename)
+    agent.model.load_state_dict(checkpoint['model_state_dict'])
+    agent.target_model.load_state_dict(checkpoint['target_model_state_dict'])
+    agent.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    agent.memory = checkpoint['memory']
+    agent.epsilon = checkpoint['epsilon']
+
+
+def save_checkpoint(agent, filename='checkpoint.pth'):
+    dix = {'policy_state_dict': agent.policy_net.state_dict(),
+           'target_state_dict': agent.target_net.state_dict(),
+           'optimizer_state_dict': agent.optimizer.state_dict(),
+           'memory': agent.memory.memory,
+           'epsilon': agent.epsilon}
+    torch.save(dix, filename)
+
+
+def evaluate_policy(fname='g2048.pth'):
+    p = torch.load(fname)
+    model = p['target_state_dict']
+    env = gym.make('Game2048-v0', render_mode='human')
+    done = False
+    state, _ = env.reset()
+    while not done:
+        action = model(state).argmax()
+        ns, r, term, trunc, info = env.step(action)
+        done = term or trunc
+        state = ns
+
+
+def record_video():
+    height, width = agent.images[0].shape[0], agent.images[0].shape[1]
+    video = cv2.VideoWriter('dqn2048.mp4',
+                            cv2.VideoWriter_fourcc(*'mp4v'),
+                            4,
+                            (width, height))
+    for img in agent.images:
+        video.write(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+    video.release()
