3eed108815e034187b6a9078f82f461a6ffc6434
Noe Javet <np.javet@gmail.com>
2025-01-11 15:08:19 +0100

    wip

--- lunar/utils/training.py
+++ lunar/utils/training.py
@@ -1,4 +1,37 @@
 import torch
+import numpy as np
+import gymnasium as gym
+
+
+def train_agent(agent, env, max_time_steps, n_envs):
+    episode_rewards = np.zeros(n_envs)
+    states = env.reset()
+    for step in range(max_time_steps):
+        actions = agent.select_actions(states)
+        next_states, rewards, dones, infos = env.step(actions)
+        episode_rewards += rewards
+        agent.store_transitions(states, actions, rewards, next_states, dones)
+        agent.learn()
+        states = next_states
+        if step % 1000 == 0:
+            agent.update_target_net()
+        if step % 1000 == 0:
+            print(f'step: {step}, rewards: {np.mean(episode_rewards)}')
+            print(f'eps', agent.epsilon)
+    torch.save(agent.target_net.state_dict(), 'lunar.pth')
+
+
+def evaluate_policy(fname='lunar.pth'):
+    p = torch.load(fname)
+    model = p['target_state_dict']
+    env = gym.make('LunarLander-v3', render_mode='human')
+    done = False
+    state, _ = env.reset()
+    while not done:
+        action = model(state).argmax()
+        ns, r, term, trunc, info = env.step(action)
+        done = term or trunc
+        state = ns
 
 
 def load_checkpoint(agent, filename='checkpoint.pth'):
---
+++ lunar/utils/vis.py
@@ -0,0 +1,9 @@
+def record_video():
+    height, width = agent.images[0].shape[0], agent.images[0].shape[1]
+    video = cv2.VideoWriter('dqn2048.mp4',
+                            cv2.VideoWriter_fourcc(*'mp4v'),
+                            4,
+                            (width, height))
+    for img in agent.images:
+        video.write(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+    video.release()
